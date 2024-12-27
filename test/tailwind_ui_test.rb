f90eba681dfadafdff77869e3150db6f9c5b9cc3
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-12-27 16:01:12 -0500

    Remove unused test

--- test/tailwind_ui_test.rb
+++ test/tailwind_ui_test.rb
@@ -6,9 +6,4 @@ class TailwindUiTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::TailwindUi::VERSION
   end
-
-  def test_initialization
-    jsx_to_erb = TailwindUi::JsxToErb.new("file contents")
-    assert_instance_of TailwindUi::JsxToErb, jsx_to_erb
-  end
 end
