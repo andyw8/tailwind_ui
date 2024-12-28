bb713a1e84797a46f24e03020fb26c50962d20a8
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-12-27 19:56:34 -0500

    Handle more complications

--- lib/tailwind_ui/jsx_to_erb.rb
+++ lib/tailwind_ui/jsx_to_erb.rb
@@ -7,6 +7,15 @@ module TailwindUi
   class NotYetSupported < TailwindUi::Error
   end
 
+  class UnconvertedBraces < TailwindUi::Error
+  end
+
+  class ClipPathNotYetSupported < TailwindUi::Error
+  end
+
+  class Special < TailwindUi::Error
+  end
+
   class JsxToErb
     def self.from_path(path)
       file_contents = File.read(path)
@@ -14,24 +23,59 @@ module TailwindUi
     end
 
     def initialize(file_contents)
-      @file_contents = file_contents
+      if file_contents.include?("This example requires updating your template")
+        raise Special
+      end
+
       unless file_contents.lines.first.start_with?("export default function")
         raise NotYetSupported
       end
+
+      if file_contents.include?("clipPath")
+        raise ClipPathNotYetSupported
+      end
+
+      @file_contents = file_contents
     end
 
     def full
       tags = @file_contents.match(/(?<tags> *<.*>)/m)[:tags]
       result = without_indentation(tags)
+      result = handle_style_attributes(result)
       result = with_class_name_conversion(result)
+      result = handle_jsx_comments(result)
+      result = handle_brace_attributes(result)
+      result = handle_inner_braces(result)
+      raise UnconvertedBraces if result.include?("{") || result.include?("}")
+
       ERB.new(result).result
+
       result
     end
 
     private
 
+    def handle_jsx_comments(markup)
+      markup
+        .gsub("{/*", "<%#")
+        .gsub("*/}", "%>")
+    end
+
+    def handle_brace_attributes(markup)
+      markup
+        .gsub(/(\w+)=\{(\d+)}/m, '\1="\2"')
+    end
+
+    def handle_style_attributes(markup)
+      markup.gsub(/style={{ (.*) }}/, 'style="\1"')
+    end
+
+    def handle_inner_braces(markup)
+      markup.gsub("{", "<%= ").gsub("}", " %>")
+    end
+
     def with_class_name_conversion(markup)
-      markup.gsub!(/className="([^"]+)"/, 'class="\1"')
+      markup.gsub(/className="([^"]+)"/, 'class="\1"')
     end
 
     def without_indentation(tags)
--- test/tailwind_ui/jsx_to_erb_test.rb
+++ test/tailwind_ui/jsx_to_erb_test.rb
@@ -48,12 +48,93 @@ class JsxToErbTest < Minitest::Test
     assert_equal expected, result
   end
 
+  def test_images
+    code = <<~JSX
+      export default function Example() {
+        return (
+          <img
+            width={158}
+            height={48}
+          />
+        )
+      }
+    JSX
+
+    expected = <<~JSX
+      <img
+        width="158"
+        height="48"
+      />
+    JSX
+
+    result = TailwindUi::JsxToErb.new(code).full
+
+    assert_equal expected, result
+  end
+
+  def test_style
+    code = <<~JSX
+      export default function Example() {
+        return (
+          <div style={{ width: '37.5%' }} />
+        )
+      }
+    JSX
+
+    expected = <<~HTML
+      <div style="width: '37.5%'" />
+    HTML
+
+    result = TailwindUi::JsxToErb.new(code).full
+
+    assert_equal expected, result
+  end
+
+  def test_jsx_comments
+    code = <<~JSX
+      export default function Example() {
+        return (
+          <div>
+          {/*
+            line 1
+            line 2
+          */}
+          </div>
+        )
+      }
+    JSX
+
+    expected = <<~JSX
+      <div>
+      <%#
+        line 1
+        line 2
+      %>
+      </div>
+    JSX
+
+    result = TailwindUi::JsxToErb.new(code).full
+
+    assert_equal expected, result
+  end
+
   def test_against_real_tailwind_ui
     skip "Skipping since SOURCE_PATH is not set" unless ENV["SOURCE_PATH"]
 
     Dir.glob("#{ENV.fetch("SOURCE_PATH")}/**/*.jsx").each do |path|
       TailwindUi::JsxToErb.from_path(path).full
+      # puts "OK: #{path}"
     rescue TailwindUi::NotYetSupported
+      # puts "Not supported yet: #{path}"
+      next
+    rescue TailwindUi::Special
+      # puts "Special: #{path}"
+      next
+    rescue SyntaxError
+      puts "Syntax: #{path}"
+      next
+    rescue TailwindUi::ClipPathNotYetSupported
+      puts "Clippath not yet supported: #{path}"
       next
     end
   end
